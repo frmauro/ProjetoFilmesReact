8bc336e0989a1a85b1deaad0122882f3c7ce5865
francisco <frmauro8@hotmail.com>
2020-02-08 09:02:50 -0300

    inclusão de lista de filmes e pagina de erro

---
+++ src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './home.css';
+
+class Home extends Component{
+
+constructor(props){
+    super(props);
+    this.state = {
+        filmes: []
+    };
+
+    this.loadFilmes = this.loadFilmes.bind(this);
+}
+
+
+componentDidMount(){
+    this.loadFilmes();
+}
+
+loadFilmes(){
+    // url da api https://sujeitoprogramador.com/r-api/?api=filmes
+    let url = "https://sujeitoprogramador.com/r-api/?api=filmes";
+    fetch(url)
+    .then((r) => r.json())
+    .then((json) => {
+        this.setState({filmes: json});
+        console.log(json);
+    });
+}
+
+    render(){
+        return(
+            <div className="container">
+                <div className="listaFilmes">
+                    { this.state.filmes.map((filme) => {
+                        return(
+                            <article key={filme.id} className="filme" >
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} />
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
